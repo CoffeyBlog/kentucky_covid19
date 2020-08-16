293073e017ca64cd274924db7c978f119f342937
coffey <lucascoffey@gmail.com>
2020-08-16 11:16:43 -0400

    Initial commit

---
+++ ky_covid_data.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import plotly.offline as pyo
+import plotly.graph_objs as go
+
+df = pd.read_csv('daily.csv')
+
+fig_inc = go.Figure(go.Line(x=df['date'], y=df['positive'], name='Infected', mode='lines+markers',
+                            marker=dict(size=10, color='indianred')))
+
+fig_inc.add_trace(go.Line(x=df['date'], y=df['recovered'], name='Recovered', mode='lines+markers',
+                          marker=dict(size=10, color='lightseagreen')))
+
+fig_inc.add_trace(
+    go.Line(x=df['date'], y=df['death'], name='Deaths', mode='lines+markers', marker=dict(size=10, color='gray')))
+
+fig_inc.add_trace(go.Line(x=df['date'], y=df['positiveIncrease'], name='Daily', mode='lines+markers',
+                          marker=dict(size=10, color='Orange')))
+
+
+
+# here we define layout of the chart
+fig_inc.update_layout(xaxis_showgrid=True, yaxis_showgrid=True, plot_bgcolor='whitesmoke',
+                      title={
+                          'text': 'Kentucky COVID-19 Cases',
+                          'y': 0.75,
+                          'x': 0.5,
+                          'xanchor': 'center',
+                          'yanchor': 'top'}, xaxis_type='category')
+fig_inc.update_xaxes(title='------>Timeline', showline=False)
+fig_inc.update_yaxes(title='------>Number of incremental cases', showline=False)
+
+fig_inc.show()
